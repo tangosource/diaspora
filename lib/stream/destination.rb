8b9ea03077ba45c0d961c0e75b21745a6daadbdc
Marco Gallardo <marco.gallardo@tangosource.com>
2012-02-03 18:52:07 -0600

    showing destination related places in destination show view

--- lib/stream/destination.rb
+++ lib/stream/destination.rb
@@ -1,10 +1,21 @@
 class Stream::Destination < Stream::Tag
+
+  RELATED_PLACES_TAGS = %w( hotels restaurants clubs attractions parks museums )
+
   def related_articles
     @related_articles ||= Magazine::Article.tagged_with(tag_name)
   end
 
   def related_places
-    @related_places ||= ::Place.tagged_with(tag_name)
+    RELATED_PLACES_TAGS.each do |tag|
+      related_places[tag] = related_places_each(tag)
+    end
+    return related_places
+  end
+
+  def related_places_each(key)
+    raise 'invalid key' unless RELATED_PLACES_TAGS.include?(key)
+    ::Place.tagged_with([tag_name,key], :match_all=>true)
   end
 
 end

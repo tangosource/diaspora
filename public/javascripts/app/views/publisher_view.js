c8625e389bcbc8013a0af5b75151f3771aac053e
Narciso Guillen <narciso.guillen@tangosource.com>
2012-02-06 19:53:59 -0600

    Fixed root issue of place button

--- public/javascripts/app/views/publisher_view.js
+++ public/javascripts/app/views/publisher_view.js
@@ -66,10 +66,9 @@ app.views.Publisher = Backbone.View.extend({
     this.$("textarea.ac_input").css('min-height', '42px');
 
     var empty = $('#status_message_fake_text').val();
+
     if((this.id) && (empty =="")){
-      $.getJSON('get_place/'+this.id,undefined, function(data) {
         var places = new Places(data);
-      })
     }
     
     return this;

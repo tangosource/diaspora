40ab58412eb41ffdf2458e3521ba9acef86c00ae
Narciso Guillen <narciso.guillen@tangosource.com>
2012-02-16 12:57:47 -0600

    Build uniq search for multiple stuff

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -14,8 +14,32 @@ class DestinationsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json do
+
         @destinations = Destination.search params[:q], params[:limit]
-        render :json => @destinations.to_json 
+        @places = Place.search params[:q], params[:limit]
+        @people = Person.search params[:q], params[:limit]        
+
+        @all = []
+
+        if @destinations.size > 0
+          @destinations.each do |destination|
+            @all.push(destination)
+          end
+        end
+
+        if @places.size > 0
+          @places.each do |place|
+            @all.push(place)
+          end
+        end
+
+        if @people.size > 0
+          @people.each do |person|
+            @all.push(person)
+          end
+        end
+
+        render :json => @all.to_json 
       end
     end
   end

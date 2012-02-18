d07d84671d6ca259e5c46a148c2fc4b77cb4b0ad
Narciso Guillen <narciso.guillen@tangosource.com>
2012-02-17 18:15:41 -0600

    Added migration for search string on places

---
+++ db/migrate/20120217231846_add_search_string_to_places.rb
@@ -0,0 +1,17 @@
+class AddSearchStringToPlaces < ActiveRecord::Migration
+  def self.up
+    add_column :places, :search_string, :string
+    Place.reset_column_information
+
+    Place.find_in_batches do |places
+      places.each do |place|
+        print '.'
+        place.save
+      end
+    end
+  end
+
+  def self.down
+    remove_column :places, :search_string 
+  end
+end

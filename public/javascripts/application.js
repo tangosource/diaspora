783167203def177fabf6bc5bfd16250c6f75f9d7
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2012-02-22 18:11:50 -0600

    [#25032783] mark SOME(after document ready ones missing) external links as rel nofollow

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,5 +6,8 @@ $(document).ready( function(){
   //Toggles places under Places section in Destinations show
   $('ul#places_list a.home_selector').click(function(){
     $(this).next().slideToggle('slow');
-  })
+  });
+
+  $("a[href^='http://']").attr("rel","nofollow");
+  $("a[href^='http://']").css("background-color","yellow");
 });

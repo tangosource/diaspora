b1ac5d5d412eefda5fe994b500868c002f2a543d
Federico Ramallo <framallo@gmail.com>
2012-03-02 12:06:29 -0600

    improved exception handling

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,4 +1,5 @@
 Diaspora::Application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[gtb][#{Rails.env}] ",
   :sender_address => %{"notifier" <no-reply@joindiaspora.com>},
+  :normalize_subject => true,
   :exception_recipients => %w{ nacho@tangosource.com federico@tangosource.com galensanford@gmail.com marco.gallardo@tangosource.com }

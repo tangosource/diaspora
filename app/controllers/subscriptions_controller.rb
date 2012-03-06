2b54d48522002c486327b00b332c2efc206a1803
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-06 12:00:30 -0600

    creating and saving subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,4 +6,13 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.new
   end
 
+  def create
+    @subscription = Subscription.new(params[:subscription])
+    @subscription.user_id = current_user.id
+    if @subscription.save!
+      redirect_to subscriptions_path, :notice => 'Thank you for subscribing!'
+    else
+      render :new, :notice => 'There was a problem when creating your subscription, try it again please.'
+    end
+  end
 end

86a0fc013a847ff73890175edc4d57714b8a31d0
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-06 12:36:11 -0600

    created method to save Stripe's customer

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,17 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
 
   attr_accessor :stripe_card_token
+
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(:description => current_user.email, :plan => plan_id, :card => stripe_card_token)
+      stripe_customer_token = customer.id
+      save!
+    end
+    rescue Stripe::InvalidRequestError => e
+      logger.error "Stripe error when creating customer: #{e.message}"
+      errors.add :base, "There was a problem with your credit card."
+      false
+  end
+
 end

5ca89ca4bcc81653952dfb4b8914eac8d52d5061
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-07 16:46:25 -0600

    moved stripe's keys into application.yaml

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key    = "khtHJo9VHJc6g9OhGOHevzMVHj7b7uJu"
-STRIPE_PUBLIC_KEY = "pk_sZx9DwtxMrFpu44SlTirc2CRx3LhG"
+Stripe.api_key    = AppConfig[:stripe_api_key]
+# also notice stripe_api_key

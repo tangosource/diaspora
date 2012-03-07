5c73a99ddc8f41dc880545594e993a1efc9f8bc5
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-07 15:31:12 -0600

    creating plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -10,4 +10,16 @@ class PlansController < ApplicationController
     end
   end
 
+  def new
+    @plan =  Plan.new
+  end
+
+  def create
+    if Plan.create(params[:plan])
+      redirect_to plans_path, :notice => 'Plan created successfuly.'
+    else
+      render :new, :notice => 'There was a problem when creating the plan.'
+    end
+  end
+
 end

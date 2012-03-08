9303f0a6e28572a7edaba5f7b509dec857f9408d
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-08 11:38:05 -0600

    fixed redirecting when saving subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -14,16 +14,16 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.new(params[:subscription])
     @subscription.user_id = current_user.id
     if @subscription.save_with_payment
-      redirect_to plans_path, :notice => 'Thank you for subscribing!'
+      redirect_to subscriptions_path, :notice => 'Thank you for subscribing!'
     else
-      redirect_to plans_path, :notice => 'There was a problem when creating your subscription, try it again please.'
+      redirect_to subscriptions_path, :notice => 'There was a problem when creating your subscription, try it again please.'
     end
   end
 
   def destroy
     @subscription = Subscription.find(params[:id])
     @subscription.delete_with_payment
-    redirect_to plans_path
+    redirect_to subscriptions_path
   end
 
 end

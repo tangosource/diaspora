3c6ed32d17be9fce8101a6ed1383b9db34dee84f
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-29 13:02:08 -0600

    added marker image for publisher, showing div for address, and added functionality to close it when clicking 'X' image
    showing marker image in publisher
    
    renamed map-marker to locator
    
    fixed style for map marker image in publisher
    
    added locator.js to get user's location
    
    loading google maps api
    
    removed unecessary append and showing location next to publisher
    
    moved location address inside div and  added image to close location
    
    styled div location address an image to close location
    
    removing location when clicking close image
    
    cleaned code
    
    cleaned code

---
+++ public/javascripts/locator.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+  function errorGettingPosition(err)
+  {
+    if(err.code==1)
+      {
+        alert("User denied geolocation.");
+      }
+      else if(err.code==2)
+        {
+          alert("Position unavailable.");
+        }
+        else if(err.code==3)
+          {
+            alert("Timeout expired.");
+          }
+          else
+            {
+              alert("ERROR:"+ err.message);
+            }
+  }
+
+  function getAddress(coords) {
+    latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          $('#publisher_textarea_wrapper').after('<div id="location"></div>');
+          $('#location').append('<div id="location_address">' + results[0].formatted_address + '</div>');
+          $('#location').append('<a id="hide_location"><img alt="delete location" src="/images/deletelabel.png"></a>');
+          bindClickToHideLocation();
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    })
+  }
+
+  function success(position) {
+    getAddress(position.coords);
+  }
+
+  function error(msg) {
+    errorGettingPosition(msg);
+  }
+
+  $("#locator").click(function(){
+    geocoder = new google.maps.Geocoder();
+    navigator.geolocation.getCurrentPosition(success, error);
+  });
+
+  function bindClickToHideLocation(){
+    $('#hide_location').click(function(){
+      $('#location').remove();
+    });
+  }
+
+});

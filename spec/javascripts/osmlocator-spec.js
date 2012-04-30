2060bb0e23525e2beb00e06c38371f172935dc7c
Daniel Lopez <daniel.lopez@tangosource.com>
2012-04-30 11:59:52 -0500

    adding Jasmine test to OSM locator

--- spec/javascripts/osmlocator-spec.js
+++ spec/javascripts/osmlocator-spec.js
@@ -1,51 +1,38 @@
-describe("Locator", function(){
+describe("Locator using OSM", function(){
+  var osmlocator = new OSM.Locator();
 
-  it("should return an especific address and a coords with OSM", function(){
- 
-    var osmlocator = new OSM.Locator();
-
-    navigator.geolocation['getCurrentPosition'] = function(cb){
-      return cb({
+  navigator.geolocation['getCurrentPosition'] = function(position){
+    return position({
+      position: {
         coords:{
           latitude: "38.8950849", 
           longitude: "-77.0365515"
-        }
-      });
-    };
+        }}
+    });
+  };
 
-    osmlocator.getAddress = function(cb){
-      return cb({
-        "place_id": "6624001",
-        "licence": "Data Copyright OpenStreetMap Contributors, Some Rights Reserved. CC-BY-SA 2.0.",
-        "osm_type": "node",
-        "osm_id": "622025590",
-        "lat": "38.8950849",
-        "lon": "-77.0365515",
-        "display_name": "Zero Milestone, Ellipse Road Northwest, Foggy Bottom, Farragut Square, Washington, Montgomery, District of Columbia, 20006, United States of America",
-        "address":{
-          "highway": "Zero Milestone",
-          "road": "Ellipse Road Northwest",
-          "place": "Foggy Bottom",
-          "suburb": "Farragut Square",
-          "city": "Washington",
-          "county": "Montgomery",
-          "state": "District of Columbia",
-          "postcode": "20006",
-          "country": "United States of America",
-          "country_code":"us"
-        }
-      });
-    };
+  //You can add those fields that you need to check
+  $.getJSON = function(data){
+    return data({
+      display_name: "Zero Milestone, Ellipse Road Northwest, Foggy Bottom, Farragut Square, Washington, Montgomery, District of Columbia, 20006, United States of America",
+      position:{
+        coords:{
+          latitude: "38.8950849", 
+          longitude: "-77.0365515"
+        }}
+    });
+  }    
 
-    var position = navigator.geolocation.getCurrentPosition(function(coords){return coords});
-    var osmlocation = osmlocator.getAddress(function(address){return address});
-    expect(position.coords.latitude).toBe("38.8950849");
-    expect(position.coords.longitude).toBe("-77.0365515");
+  var browser_navigator = navigator.geolocation.getCurrentPosition(function(coords){return coords});
+  var data = $.getJSON(function(data){return data});
 
-    expect(osmlocation.display_name).toBe("Zero Milestone, Ellipse Road Northwest, Foggy Bottom, Farragut Square, Washington, Montgomery, District of Columbia, 20006, United States of America");
-    expect(osmlocation.lat).toBe("38.8950849");
-    expect(osmlocation.lon).toBe("-77.0365515");
-    expect(osmlocation.address.city).toBe("Washington");
-    expect(osmlocation.address.country).toBe("United States of America");
+  it("should return an especific position with navigator method", function(){
+    expect(browser_navigator.position.coords.latitude).toEqual("38.8950849");
+    expect(browser_navigator.position.coords.longitude).toEqual("-77.0365515");
+  });
+  it("should return an especific display name and position using getJSON method using OSM service", function(){
+    expect(data.display_name).toEqual("Zero Milestone, Ellipse Road Northwest, Foggy Bottom, Farragut Square, Washington, Montgomery, District of Columbia, 20006, United States of America");
+    expect(data.position.coords.latitude).toEqual("38.8950849");
+    expect(data.position.coords.longitude).toEqual("-77.0365515");
   });
 });
